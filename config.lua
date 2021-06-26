24a426cc844f467de653b6d8e7b216667e6401c0
viruspc2002 <79900651+viruspc2002@users.noreply.github.com>
2021-06-26 15:18:11 +0530

    update to vector4

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ QBConfig = {}
 
 QBConfig.MaxPlayers = GetConvarInt('sv_maxclients', 64) -- Gets max players from config file, default 32
 QBConfig.IdentifierType = "steam" -- Set the identifier type (can be: steam, license)
-QBConfig.DefaultSpawn = {x=-1035.71,y=-2731.87,z=12.86,a=0.0}
+QBConfig.DefaultSpawn = vector4(-1035.71, -2731.87, 12.86, 0.0)
 
 QBConfig.Money = {}
 QBConfig.Money.MoneyTypes = {['cash'] = 500, ['bank'] = 5000, ['crypto'] = 0 } -- ['type']=startamount - Add or remove money types for your server (for ex. ['blackmoney']=0), remember once added it will not be removed from the database!
