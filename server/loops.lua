6ef71fbc0e81b933919ae767a425bb3bfd148306
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-07-30 21:04:50 +0200

    Rename function to be called in the loop

--- server/loops.lua
+++ server/loops.lua
@@ -9,5 +9,5 @@ PaycheckLoop = function()
             TriggerClientEvent('QBCore:Notify', Players[i], "You received your paycheck of $"..Player.PlayerData.job.payment)
         end
     end
-    SetTimeout(QBCore.Config.Money.PayCheckTimeOut * (60 * 1000), GivePayCheck)
+    SetTimeout(QBCore.Config.Money.PayCheckTimeOut * (60 * 1000), PaycheckLoop)
 end
