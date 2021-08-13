d11556f116cd0305205730016886b3331350923a
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-08-14 01:44:41 +0200

    Change message for admins

--- server/commands.lua
+++ server/commands.lua
@@ -151,7 +151,7 @@ QBCore.Commands.Add("ooc", "OOC Chat Message", {}, false, function(source, args)
 	for k, v in pairs(QBCore.Functions.GetPlayers()) do
 		if QBCore.Functions.HasPermission(v, "admin") then
 			if QBCore.Functions.IsOptin(v) then
-				TriggerClientEvent('chatMessage', v, "OOC " .. GetPlayerName(source), "normal", message)
+				TriggerClientEvent('chatMessage', v, "Proximity OOC " .. GetPlayerName(source), "normal", message)
 				TriggerEvent("qb-log:server:CreateLog", "ooc", "OOC", "white", "**"..GetPlayerName(source).."** (CitizenID: "..Player.PlayerData.citizenid.." | ID: "..source..") **Message:** " ..message, false)
 			end
 		elseif #(GetEntityCoords(GetPlayerPed(source)) - GetEntityCoords(GetPlayerPed(v))) < 20.0 then
