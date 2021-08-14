d2386910b856f0a377e766a5ef7007a8e84505d9
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-08-14 15:37:20 +0200

    Check distance first instead of admin to seperate admin ooc and proximity

--- server/commands.lua
+++ server/commands.lua
@@ -151,13 +151,13 @@ QBCore.Commands.Add("ooc", "OOC Chat Message", {}, false, function(source, args)
 	for k, v in pairs(QBCore.Functions.GetPlayers()) do
 		if v == source then
 			TriggerClientEvent('chatMessage', v, "OOC " .. GetPlayerName(source), "normal", message)
+		elseif #(GetEntityCoords(GetPlayerPed(source)) - GetEntityCoords(GetPlayerPed(v))) < 20.0 then
+			TriggerClientEvent('chatMessage', v, "OOC " .. GetPlayerName(source), "normal", message)
 		elseif QBCore.Functions.HasPermission(v, "admin") then
 			if QBCore.Functions.IsOptin(v) then
 				TriggerClientEvent('chatMessage', v, "Proximity OOC " .. GetPlayerName(source), "normal", message)
 				TriggerEvent("qb-log:server:CreateLog", "ooc", "OOC", "white", "**"..GetPlayerName(source).."** (CitizenID: "..Player.PlayerData.citizenid.." | ID: "..source..") **Message:** " ..message, false)
 			end
-		elseif #(GetEntityCoords(GetPlayerPed(source)) - GetEntityCoords(GetPlayerPed(v))) < 20.0 then
-			TriggerClientEvent('chatMessage', v, "OOC " .. GetPlayerName(source), "normal", message)
 		end
 	end
 end)
