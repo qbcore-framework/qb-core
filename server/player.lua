3800de1267c8b3ef8297d2450086ace362b764e3
David <88985795+davidecab@users.noreply.github.com>
2021-09-02 23:46:11 +0200

    Notify if inventory is full

--- server/player.lua
+++ server/player.lua
@@ -321,6 +321,8 @@ QBCore.Player.CreatePlayer = function(PlayerData)
 					end
 				end
 			end
+		else
+			TriggerClientEvent('QBCore:Notify', self.PlayerData.source, "Inventory is full!", "error")
 		end
 		return false
 	end
