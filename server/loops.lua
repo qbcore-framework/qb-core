c4042a7048dc976a6632ae1daae39b3314f7532b
kShady <64763166+p4zinee@users.noreply.github.com>
2021-09-17 07:06:34 +0700

    job payment when offduty
    cannot receive payment from job when offduty

--- server/loops.lua
+++ server/loops.lua
@@ -5,8 +5,12 @@ PaycheckLoop = function()
         local Player = QBCore.Functions.GetPlayer(Players[i])
 
         if Player.PlayerData.job ~= nil and Player.PlayerData.job.payment > 0 then
-            Player.Functions.AddMoney('bank', Player.PlayerData.job.payment)
-            TriggerClientEvent('QBCore:Notify', Players[i], "You received your paycheck of $"..Player.PlayerData.job.payment)
+            if Player.PlayerData.job.onduty then 
+                Player.Functions.AddMoney('bank', Player.PlayerData.job.payment)
+                TriggerClientEvent('QBCore:Notify', Players[i], "You received your paycheck of $"..Player.PlayerData.job.payment)
+            else
+                TriggerClientEvent('QBCore:Notify', Players[i], "You are not on duty and will not receive payment from your job!")
+            end
         end
     end
     SetTimeout(QBCore.Config.Money.PayCheckTimeOut * (60 * 1000), PaycheckLoop)
