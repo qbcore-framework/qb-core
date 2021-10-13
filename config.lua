e719bc6569dd68a7b95512c6f3e9cd70297137e8
Taso <taso@projecterror.dev>
2021-10-13 19:39:59 -0400

    refactor(ui/notify): Cleanup internal code & new config options

--- config.lua
+++ config.lua
@@ -33,3 +33,37 @@ QBConfig.Server.uptime = 0 -- Time the server has been up.
 QBConfig.Server.whitelist = false -- Enable or disable whitelist on the server
 QBConfig.Server.discord = "" -- Discord invite link
 QBConfig.Server.PermissionList = {} -- permission list
+
+QBConfig.Notify = {}
+
+QBConfig.Notify.NotificationStyling = {
+    group = false,              -- Allow notifications to stack with a badge instead of repeating
+    position = "right",         -- top-left | top-right | bottom-left | bottom-right | top | bottom | left | right | center
+    progress = true             -- Display Progress Bar
+}
+
+-- These are how you define different notification variants
+-- The "color" key is background of the notification
+-- The "icon" key is the css-icon code, this project uses `Material Icons` & `Font Awesome`
+QBconfig.Notify.VariantDefinitions = {
+    success = {
+        color = 'green',
+        icon = 'done'
+    },
+    primary = {
+        color = 'blue',
+        icon = 'info'
+    },
+    error = {
+        color = 'red',
+        icon = 'dangerous'
+    },
+    police = {
+        color = 'blue',
+        icon = 'local_police'
+    },
+    ambulance = {
+        color = 'red',
+        icon = 'fas fa-ambulance'
+    }
+}
---
+++ html/config.js
@@ -0,0 +1,46 @@
+import { fetchNui, isEnvBrowser } from "./utils.js";
+import { BrowserMockConfigData } from "./testing.js";
+
+export const DEV_MODE = false;
+
+/**
+ * @typedef NotiVariantData
+ * @property {string} icon
+ * @property {string} color
+ **/
+
+/**
+ * Will hold config statically outside of Vue state
+ * @property {Record<string, NotiVariantData>} VariantDefinitions
+ * @property {Record<string, any>} NotificationStyling
+ * */
+export let NOTIFY_CONFIG = null;
+
+/**
+ * Pure function taking a notification type and returning an object
+ * with style details
+ * @param variant {string}
+ * @returns NotiVariantData
+ **/
+export const determineStyleFromVariant = (variant) => {
+  const variantData = NOTIFY_CONFIG.VariantDefinitions[variant];
+  if (!variantData)
+    throw new Error(`Style of type: ${variant}, does not exist in the config`);
+  return variantData;
+};
+
+// Fetch and set NOTIFY_CONFIG from client script callback
+export const fetchNotifyConfig = async () => {
+  NOTIFY_CONFIG = await fetchNui("getNotifyConfig", {}, BrowserMockConfigData);
+  if (isEnvBrowser() || DEV_MODE) {
+    console.log("Fetched Config:");
+    console.dir(NOTIFY_CONFIG);
+  }
+};
+
+// We specifically wait for all other files to load
+// just in case of a race condition between client handlers
+// and NUI fetch call
+window.addEventListener("load", async () => {
+  await fetchNotifyConfig();
+});
