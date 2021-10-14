28ba599019578a709a5379d428174bd7ee5b0ed1
Taso <taso@projecterror.dev>
2021-10-13 21:39:57 -0400

    fix(ui/notify): fix wrapper definition for fetchApi

--- html/utils.js
+++ html/utils.js
@@ -15,11 +15,12 @@ export const fetchNui = async (evName, data, mockData = null) => {
 
   const resourceName = window.GetParentResourceName();
 
-  const rawResp = await fetch(`https://cfx-nui-${resourceName}/${evName}`, {
+  const rawResp = await fetch(`https://${resourceName}/${evName}`, {
     body: JSON.stringify(data),
     headers: {
       "Content-Type": "application/json; charset=UTF8",
     },
+    method: "POST",
   });
 
   return await rawResp.json();
