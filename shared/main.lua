deae13cb797f10ef4eea8a9df05ee8dcf9292682
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2022-01-03 09:10:06 +0200

    fix name

--- shared/main.lua
+++ shared/main.lua
@@ -76,10 +76,10 @@ QBShared.StarterItems = {
     ['driver_license'] = { amount = 1, item = 'driver_license' },
 }
 
-QBShared.MaleNoHandshoes = {
+QBShared.MaleNoGloves = {
     [0] = true, [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [18] = true, [26] = true, [52] = true, [53] = true, [54] = true, [55] = true, [56] = true, [57] = true, [58] = true, [59] = true, [60] = true, [61] = true, [62] = true, [112] = true, [113] = true, [114] = true, [118] = true, [125] = true, [132] = true
 }
 
-QBShared.FemaleNoHandshoes = {
+QBShared.FemaleNoGloves = {
     [0] = true, [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [19] = true, [59] = true, [60] = true, [61] = true, [62] = true, [63] = true, [64] = true, [65] = true, [66] = true, [67] = true, [68] = true, [69] = true, [70] = true, [71] = true, [129] = true, [130] = true, [131] = true, [135] = true, [142] = true, [149] = true, [153] = true, [157] = true, [161] = true, [165] = true
 }
