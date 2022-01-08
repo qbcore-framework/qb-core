84b2e4dc110862b7f77014e4e97c9d16ca52b1b9
StuwStuw <51363575+StuwStuw@users.noreply.github.com>
2022-01-07 21:55:50 -0700

    Feat. Main Test Command
    Implemented test command to main.lua on client side. Allows user to check if commands are working properly.

--- client/main.lua
+++ client/main.lua
@@ -8,6 +8,11 @@ exports('GetCoreObject', function()
     return QBCore
 end)
 
+RegisterCommand('test', function()
+    local Player = QBCore.Functions.GetPlayerData()
+    print('^6[^4QB-Core^6]', ('%s %s has executed Hello World!'):format(Player.charinfo.firstname, Player.charinfo.lastname))
+end)
+
 -- To use this export in a script instead of manifest method
 -- Just put this line of code below at the very top of the script
 -- local QBCore = exports['qb-core']:GetCoreObject()
