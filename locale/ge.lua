48b46c7c0085c2017b8b95c820ff1aab5e419780
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-01-11 07:01:52 -0600

    Georgian Language ge.lua
    Georgian Language Locale File. as many Georgian servers coming.

---
+++ locale/ge.lua
@@ -0,0 +1,25 @@
+local Translations = {
+    error = {
+        not_online = 'მოთამაშე ონლაინში არ არის',
+        wrong_format = 'არასწორი ფორმატი',
+        missing_args = 'ყველა არგუმენტი არ არის შეყვანილი (x, y, z)',
+        missing_args2 = 'ყველა არგუმენტი უნდა იყოს შევსებული!',
+        no_access = 'ამ ბრძანებაზე წვდომა არ არის',
+        company_too_poor = 'თქვენი დამსაქმებელი ღარიბია',
+        item_exist = 'ნივთი არ არსებობს',
+        too_heavy = 'ზედმეტად სავსეა ინვენტარი'
+    },
+    success = {},
+    info = {
+        received_paycheck = 'თქვენ მიიღეთ თქვენი ხელფასი $%{value}',
+        job_info = 'სამუშაო: %{value} | შეფასება: %{value2} | მოვალეობა: %{value3}',
+        gang_info = 'ჯგუფი: %{value} | შეფასება: %{value2}',
+        on_duty = 'ახლა მორიგე ხარ!',
+        off_duty = 'ახლა სამსახურიდან გასული ხარ!'
+    }
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
