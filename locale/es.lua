9193b87d27ce8c60580946908f08a59e4bf9ceed
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2022-01-13 00:38:43 -0500

    Update es.lua

--- locale/es.lua
+++ locale/es.lua
@@ -1,20 +1,20 @@
 local Translations = {
     error = {
-        not_online = 'Jugador no conectado',
-        wrong_format = 'Formato incorrecto',
-        missing_args = 'No todos los argumentos han sido rellenados (x, y, z)',
-        missing_args2 = 'Todos los argumentos deben ser rellenados!',
-        no_access = 'No tienes acceso a este comando',
-        company_too_poor = 'Tu empresa está en bancarrota',
-        item_not_exist = 'El objeto no existe',
-        too_heavy = 'Tienes el inventario lleno'
+        not_online = 'El jugador no esta conectado!',
+        wrong_format = 'Formato incorrecto!',
+        missing_args = 'No todos los argumentos estan presente! (x, y, z)',
+        missing_args2 = 'Todos los argumentos estan presente!',
+        no_access = 'No tienes acceso a este comando!',
+        company_too_poor = 'Tu empresa está en bancarrota. No hay dinero suficiente.',
+        item_not_exist = 'El objeto no existe en el systema.',
+        too_heavy = 'Tienes el inventario muy lleno.'
     },
     success = {},
     info = {
-        received_paycheck = 'Has recibido tu paga de $%{value}',
-        job_info = 'Trabajo: %{value} | Rango: %{value2} | Estado: %{value3}',
-        gang_info = 'Banda: %{value} | Rango: %{value2}',
-        on_duty = 'Ahora estás de servicio!',
+        received_paycheck = 'Has recibido tu pago de $%{value}',
+        job_info = 'Trabajo: %{value} | Grado: %{value2} | Estado: %{value3}',
+        gang_info = 'Banda: %{value} | Grado: %{value2}',
+        on_duty = 'Ahora estás en servicio!',
         off_duty = 'Ahora estás fuera de servicio!'
     }
 }
