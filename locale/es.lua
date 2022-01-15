49f8eb268f8d96135e2ddfc28305557ce022eb8e
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2022-01-15 01:45:23 -0500

    updated spanish
    Sounded weird

--- locale/es.lua
+++ locale/es.lua
@@ -3,7 +3,7 @@ local Translations = {
         not_online = 'El jugador no esta conectado!',
         wrong_format = 'Formato incorrecto!',
         missing_args = 'No todos los argumentos estan presente! (x, y, z)',
-        missing_args2 = 'Todos los argumentos estan presente!',
+        missing_args2 = 'Todos los argumentos tienen que estar presente!',
         no_access = 'No tienes acceso a este comando!',
         company_too_poor = 'Tu empresa está en bancarrota. No hay dinero suficiente.',
         item_not_exist = 'El objeto no existe en el systema.',
@@ -13,7 +13,7 @@ local Translations = {
     info = {
         received_paycheck = 'Has recibido tu pago de $%{value}',
         job_info = 'Trabajo: %{value} | Grado: %{value2} | Estado: %{value3}',
-        gang_info = 'Banda: %{value} | Grado: %{value2}',
+        gang_info = 'Pandilla: %{value} | Grado: %{value2}',
         on_duty = 'Ahora estás en servicio!',
         off_duty = 'Ahora estás fuera de servicio!'
     }
