0d449d9e0301c67487af26083eed6e8b754caf7a
FjamZoo <34508112+FjamZoo@users.noreply.github.com>
2022-01-15 04:36:42 +0100

    Updated danish translation
    There was some grammatical errors (I hope I caught them all I am dyslexic myself KekW)

--- locale/da.lua
+++ locale/da.lua
@@ -2,10 +2,10 @@ local Translations = {
     error = {
         not_online = 'Spiller ikke online',
         wrong_format = 'ugyldigt format',
-        missing_args = 'Ikke alle argumenter er udfyldt (x, y, z)',
+        missing_args = 'Alle argumenter er ikke udfyldt (x, y, z)',
         missing_args2 = 'Alle argumenter skal være udfyldt!',
         no_access = 'Ingen adgang til denne command',
-        company_too_poor = 'Din ansat er fattig',
+        company_too_poor = 'Arbejdsgiveren er for fattig',
         item_not_exist = 'Enheden eksistere ikke',
         too_heavy = 'Dit inventory er for fyldt'
     },
@@ -15,7 +15,7 @@ local Translations = {
         job_info = 'Arbejde: %{value} | Grad: %{value2} | Job: %{value3}',
         gang_info = 'Bande: %{value} | Grad: %{value2}',
         on_duty = 'Du er nu på arbejde, god arbejdslyst!',
-        off_duty = 'Du er gået af arbejde!'
+        off_duty = 'Du er ikke længere på arbejde!'
     }
 }
 
