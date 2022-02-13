7f28142b1e594af24a65c58f7603978fcfb7507f
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2022-02-13 09:39:18 +0200

    add isdead into Thread

--- client/loops.lua
+++ client/loops.lua
@@ -12,11 +12,12 @@ CreateThread(function()
     while true do
         Wait(QBCore.Config.StatusInterval)
         if LocalPlayer.state.isLoggedIn then
-            if QBCore.Functions.GetPlayerData().metadata['hunger'] <= 0 or
-                    QBCore.Functions.GetPlayerData().metadata['thirst'] <= 0 then
-                local ped = PlayerPedId()
-                local currentHealth = GetEntityHealth(ped)
-                SetEntityHealth(ped, currentHealth - math.random(5, 10))
+            if QBCore.Functions.GetPlayerData().metadata['hunger'] <= 0 or QBCore.Functions.GetPlayerData().metadata['thirst'] <= 0 then
+				if not QBCore.Functions.GetPlayerData().metadata["isdead"] then
+					local ped = PlayerPedId()
+					local currentHealth = GetEntityHealth(ped)
+					SetEntityHealth(ped, currentHealth - math.random(5, 10))
+				end
             end
         end
     end
