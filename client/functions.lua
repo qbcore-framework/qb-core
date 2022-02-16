bbbde38ab4075fbbce97350f13a7fff0d60c90a2
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2022-02-16 09:56:15 +0200

    QBCore Support Dui Functions

--- client/functions.lua
+++ client/functions.lua
@@ -824,3 +824,71 @@ function QBCore.Functions.StartParticleOnEntity(Dict, ptName, looped, entity, bo
 
     return particleHandle
 end
+
+local duiCounter = 0
+local availableDuis = {}
+local duis = {}
+
+function QBCore.Functions.GetDui(url, width, height)
+    width = width or 512
+    height = height or 512
+
+    local duiSize = tostring(width) .. 'x' .. tostring(height)
+
+    if (availableDuis[duiSize] and #availableDuis[duiSize] > 0) then -- Check if dui with size exists
+        local n,t = pairs(availableDuis[duiSize])
+        local nextKey, nextValue = n(t)
+        local id = nextValue
+        local dictionary = duis[id].textureDictName
+        local texture = duis[id].textureName
+        nextValue = nil -- clear
+        table.remove(availableDuis[duiSize], nextKey)
+        SetDuiUrl(duis[id].duiObject, url)
+        return {id = id, dictionary = dictionary, texture = texture}
+    end
+
+    duiCounter = duiCounter + 1 -- Generate a new one.
+    local generatedDictName = duiSize..'-dict-'..tostring(duiCounter)
+    local generatedTxtName = duiSize..'-txt-'..tostring(duiCounter)
+    local duiObject = CreateDui(url, width, height)
+    local dictObject = CreateRuntimeTxd(generatedDictName)
+    local duiHandle = GetDuiHandle(duiObject)
+    local txdObject = CreateRuntimeTextureFromDuiHandle(dictObject, generatedTxtName, duiHandle)
+
+    duis[duiCounter] = {
+        duiSize = duiSize,
+        duiObject = duiObject,
+        duiHandle = duiHandle,
+        dictionaryObject = dictObject,
+        textureObject = txdObject,
+        textureDictName = generatedDictName,
+        textureName = generatedTxtName
+    }
+
+    return {id = duiCounter, dictionary = generatedDictName, texture = generatedTxtName}
+end
+
+function QBCore.Functions.ChangeDuiUrl(id, url)
+    if (not duis[id]) then
+        return
+    end
+
+    local settings = duis[id]
+    SetDuiUrl(settings.duiObject, url)
+end
+
+function QBCore.Functions.ReleaseDui(id)
+    if (not duis[id]) then
+        return
+    end
+
+    local settings = duis[id]
+    local duiSize = settings.duiSize
+
+    SetDuiUrl(settings.duiObject, 'about:blank')
+    if not availableDuis[duiSize] then
+      availableDuis[duiSize] = {}
+    end
+	
+    table.insert(availableDuis[duiSize], id)
+end
