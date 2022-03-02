d224e02f2f6691b92a175edf33adbd9ad6934f9f
Braydxn <73808761+braydxnn@users.noreply.github.com>
2022-03-01 23:28:19 -0500

    Update drawtext.lua

--- client/drawtext.lua
+++ client/drawtext.lua
@@ -38,21 +38,10 @@ local function keyPressed()
     end)
 end
 
-RegisterNetEvent('qb-core:client:DrawText', function(text, position)
-    drawText(text, position)
-end)
-
-RegisterNetEvent('qb-core:client:ChangeText', function(text, position)
-    changeText(text, position)
-end)
-
-RegisterNetEvent('qb-core:client:HideText', function()
-    hideText()
-end)
-
-RegisterNetEvent('qb-core:client:KeyPressed', function()
-    keyPressed()
-end)
+RegisterNetEvent('qb-core:client:DrawText', drawText)
+RegisterNetEvent('qb-core:client:ChangeText', changeText)
+RegisterNetEvent('qb-core:client:HideText', hideText)
+RegisterNetEvent('qb-core:client:KeyPressed', keyPressed)
 
 exports('DrawText', drawText)
 exports('ChangeText', changeText)
