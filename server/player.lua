b5de979b794fe52425d080ea3955aa41bdde6db4
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-03-19 11:16:10 +0100

    fix(server/player): error on character creation

--- server/player.lua
+++ server/player.lua
@@ -116,7 +116,7 @@ function QBCore.Player.CheckPlayerData(source, PlayerData)
         InstalledApps = {},
     }
     -- Job
-    if not QBCore.Shared.Jobs[PlayerData.job.name] then PlayerData.job = nil end
+    if PlayerData.job and PlayerData.job.name and not QBCore.Shared.Jobs[PlayerData.job.name] then PlayerData.job = nil end
     PlayerData.job = PlayerData.job or {}
     PlayerData.job.name = PlayerData.job.name or 'unemployed'
     PlayerData.job.label = PlayerData.job.label or 'Civilian'
@@ -129,7 +129,7 @@ function QBCore.Player.CheckPlayerData(source, PlayerData)
     PlayerData.job.grade.name = PlayerData.job.grade.name or 'Freelancer'
     PlayerData.job.grade.level = PlayerData.job.grade.level or 0
     -- Gang
-    if not QBCore.Shared.Gangs[PlayerData.gang.name] then PlayerData.gang = nil end
+    if PlayerData.gang and PlayerData.gang.name and not QBCore.Shared.Gangs[PlayerData.gang.name] then PlayerData.gang = nil end
     PlayerData.gang = PlayerData.gang or {}
     PlayerData.gang.name = PlayerData.gang.name or 'none'
     PlayerData.gang.label = PlayerData.gang.label or 'No Gang Affiliaton'
