99af63c83c6dce29f7eeba6eaefd8e27b79688a1
El Vod Kaa <44878760+elvodkaa@users.noreply.github.com>
2022-03-19 14:45:24 +0100

    Update ar.lua

--- locale/ar.lua
+++ locale/ar.lua
@@ -1,21 +1,21 @@
 local Translations = {
     error = {
-        not_online = 'اللاعب غير متواجد',
+        not_online = 'اللاعب غير متصل',
         wrong_format = 'التنسيق غير صحيح',
-        missing_args = 'لم يتم ادخل جميع الحقول تاكد من وضع (x, y, z)',
-        missing_args2 = 'يجب ملأ جميع الحقول اللازمة!',
+        missing_args = '(x, y, z) لم يتم ادخل جميع المعلومات',
+        missing_args2 = 'يجب ملأ جميع الحقول اللازمة',
         no_access = 'لا يمكن الوصول إلى هذا الأمر',
-        company_too_poor = 'رئيس العمل الخاص بك لا يملك اموال كافية',
-        item_not_exist = 'لا يوجد هذا العنصر',
-        too_heavy = 'المحفظة ممتلئة'
+        company_too_poor = 'مسؤول الوظيفة لا يمكلك مال كاف-ي',
+        item_not_exist = 'عنصر غير موجود',
+        too_heavy = 'لا يوجد مساحة في جقيبتك'
     },
     success = {},
     info = {
-        received_paycheck = 'لقد استملت راتبك الشهري $%{value}',
-        job_info = 'الوظيفة: %{value} | الرتبة: %{value2} | الخدمة: %{value3}',
-        gang_info = 'العصابة: %{value} | الرتبة: %{value2}',
-        on_duty = 'انت الان خارج الخدمة!',
-        off_duty = 'انت الان في الخدمة!'
+        received_paycheck = '$%{value} لقد استملت راتبك الشهري',
+        job_info = '%{value} | %{value2} | %{value3}',
+        gang_info = '%{value} | %{value2}',
+        on_duty = 'انت الان خارج الخدمة',
+        off_duty = 'انت الان في الخدمة'
     }
 }
 
