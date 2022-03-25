1dfdb46105109de58963b64a99087146c83c7215
Idris Dev <IdrisDose@users.noreply.github.com>
2022-03-25 21:35:37 +1100

    fix (Zap): Fixed user not getting god perms when using Zap Hosting.

--- server/player.lua
+++ server/player.lua
@@ -45,7 +45,7 @@ function QBCore.Player.CheckPlayerData(source, PlayerData)
     PlayerData.cid = PlayerData.cid or 1
     if not QBCore.Config.Server.UseOldPermissionSystem then
         if PlayerData.permission then ExecuteCommand(('remove_principal identifier.%s group.%s'):format(PlayerData.license, PlayerData.permission)) end
-        if not PlayerData.permission and IsPlayerAceAllowed(source, 'command') and IsPlayerAceAllowed(source, 'webadmin') then -- if both aces are allowed then the person has group.admin and is a system admin
+        if not PlayerData.permission and IsPlayerAceAllowed(source, 'command') then -- if both aces are allowed then the person has group.admin and is a system admin
             PlayerData.permission = QBCore.Config.Server.AllPermissions[1] or 'god'
         end
         PlayerData.permission = PlayerData.permission or 'user'
