76551e33886794244cf27bb15c2ff77440dd395f
Millus <masterillusionist@gmail.com>
2022-03-28 22:49:21 +0200

    feat: add global switch function to qb-core

--- shared/main.lua
+++ shared/main.lua
@@ -70,6 +70,13 @@ function QBShared.SetDefaultVehicleExtras(vehicle, config)
     end
 end
 
+_G.switch = function(param, case_table)
+    local case = case_table[param]
+    if case then return case() end
+    local def = case_table['default']
+    return def and def() or nil
+end
+
 QBShared.StarterItems = {
     ['phone'] = { amount = 1, item = 'phone' },
     ['id_card'] = { amount = 1, item = 'id_card' },
