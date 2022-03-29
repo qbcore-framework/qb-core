e730b015453c7777f77147f0c6e050a399e0c574
TRClassic <82931913+trclassic92@users.noreply.github.com>
2022-03-29 00:13:51 -0400

    Loading issue
    wasn't loading the shared files in order

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,15 @@ description 'QB-Core'
 version '1.1.0'
 
 shared_scripts {
-	'shared/*.lua',
 	'config.lua',
-	'locale/en.lua' -- replace with desired language
+	'locale/en.lua', -- replace with desired language
+	'shared/main.lua',
+	'shared/items.lua',
+	'shared/jobs.lua',
+	'shared/vehicles.lua',
+	'shared/gangs.lua',
+	'shared/weapons.lua'
+}
 }
 
 client_scripts {
