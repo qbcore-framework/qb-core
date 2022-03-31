5394988ce0a93a4c20b11c96389758bc9761b7f8
jnccloud <58707473+jnccloud@users.noreply.github.com>
2022-03-31 14:45:51 -0400

    Remove compound assignment operator
    Compound assignment operators are not valid in Lua 5.4 without applying a patch (http://lua-users.org/wiki/LuaPowerPatches). This will convert this calculation back to the standard method which should not throw any errors on usage.

--- server/player.lua
+++ server/player.lua
@@ -578,7 +578,7 @@ function QBCore.Player.GetTotalWeight(items)
     local weight = 0
     if not items then return 0 end
     for _, item in pairs(items) do
-        weight += item.weight * item.amount
+        weight = weight + (item.weight * item.amount)
     end
     return tonumber(weight)
 end
