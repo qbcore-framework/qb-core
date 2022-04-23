f30f1f298e78beb670a4909b6cb6e9a59dd946bd
Dolbajoba <83533764+Getpwnedbydolbajoba@users.noreply.github.com>
2022-04-23 12:28:18 +0300

    Estonian translation.

---
+++ locale/ee.lua
@@ -0,0 +1,31 @@
+local Translations = {
+    error = {
+        not_online = 'Mängija pole serveris!',
+        wrong_format = 'Vale formaat.',
+        missing_args = 'Kõiki argumente pole sisestatud (x, y, z)',
+        missing_args2 = 'Kõik argumendid tuleb täita!',
+        no_access = 'Sellele käsule pole juurdepääsu!',
+        company_too_poor = 'Teie tööandja on pankrotis.',
+        item_not_exist = 'Sellist asja ei eksisteeri',
+        too_heavy = 'Inventuur on liiga täis',
+        duplicate_license = 'Leiti Rockstari litsentsi duplikaat',
+        no_valid_license  = 'Kehtivat Rockstari litsentsi ei leitud',
+        not_whitelisted = 'Te\'pole serveri Allowlistis!'
+    },
+    success = {},
+    info = {
+        received_paycheck = 'Saite oma töötasu kätte $%{value}',
+        job_info = 'Töökoht: %{value} | Auaste: %{value2} | Tööl: %{value3}',
+        gang_info = 'Gang: %{value} | Auaste: %{value2}',
+        on_duty = 'Alustasite enda tööpäeva!',
+        off_duty = 'Lõpetasite enda tööpäeva!',
+        checking_ban = 'Tere %s. Me kontrollime, kas olete keelustatud.',
+        join_server = 'Tere tulemast %s serverisse {Server Name}.',
+        checking_whitelisted = 'Tere %s. Kontrollime teie Allowlisti olemasolu.'
+    }
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
