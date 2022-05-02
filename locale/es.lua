1618262e21953e77d3cc4b01bf71e47a78fb9f50
buddizer <lucasg@live.com>
2022-05-02 14:05:51 -0600

    Update es.lua
    updated old phrases compared to en.lua and made them more intuitive and neutral

--- locale/es.lua
+++ locale/es.lua
@@ -1,13 +1,13 @@
 local Translations = {
     error = {
-        not_online = '¡El jugador no está conectado!',
-        wrong_format = '¡Formato incorrecto!',
-        missing_args = '¡No todos los argumentos están presentes! (x, y, z)',
+        not_online = 'El jugador no está conectado',
+        wrong_format = 'Formato incorrecto',
+        missing_args = 'No has ingresado todos los argumentos (x, y, z)',
         missing_args2 = '¡Todos los argumentos tienen que estar presentes!',
-        no_access = '¡No tienes acceso a este comando!',
-        company_too_poor = 'Tu empresa está en bancarrota. No hay dinero suficiente.',
-        item_not_exist = 'El objeto no existe en el sistema.',
-        too_heavy = 'Tienes el inventario muy lleno.',
+        no_access = 'No tienes acceso a este comando',
+        company_too_poor = 'Tu empleador está en bancarrota',
+        item_not_exist = 'El objeto no existe',
+        too_heavy = 'Inventario muy lleno',
         duplicate_license = 'Tu licencia de Rockstar está duplicada',
         no_valid_license  = 'No tienes licencia de Rockstar válida',
         not_whitelisted = 'No estás en la lista blanca de este servidor'
@@ -15,13 +15,13 @@ local Translations = {
     success = {},
     info = {
         received_paycheck = 'Has recibido tu pago de $%{value}',
-        job_info = 'Trabajo: %{value} | Grado: %{value2} | Estado: %{value3}',
-        gang_info = 'Pandilla: %{value} | Grado: %{value2}',
-        on_duty = '¡Ahora estás en servicio!',
-        off_duty = '¡Ahora estás fuera de servicio!',
+        job_info = 'Trabajo: %{value} | Puesto: %{value2} | Estado: %{value3}',
+        gang_info = 'Pandilla: %{value} | Puesto: %{value2}',
+        on_duty = '¡Estás en servicio!',
+        off_duty = '¡Estás fuera de servicio!',
         checking_ban = 'Hola %s. Estámos revisando si has sido baneado.',
         join_server = 'Bienvenido %s a {Server Name}.',
-        checking_whitelisted = 'Hola %s. Estámos revisando si estás en lista blanca.'
+        checking_whitelisted = 'Hola %s. Estámos revisando si estás en nuestra lista blanca.'
     }
 }
 
