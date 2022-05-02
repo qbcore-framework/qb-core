d32b78a9ec1c6943f0d771f9c8d8bf61813d8386
buddizer <lucasg@live.com>
2022-05-02 13:33:20 -0600

    Update es.lua
    Fixed some translation and added missing phrases

--- locale/es.lua
+++ locale/es.lua
@@ -1,10 +1,10 @@
 local Translations = {
     error = {
-        not_online = 'El jugador no está conectado!',
-        wrong_format = 'Formato incorrecto!',
-        missing_args = 'No todos los argumentos estan presente! (x, y, z)',
-        missing_args2 = 'Todos los argumentos tienen que estar presente!',
-        no_access = 'No tienes acceso a este comando!',
+        not_online = '¡El jugador no está conectado!',
+        wrong_format = '¡Formato incorrecto!',
+        missing_args = '¡No todos los argumentos están presentes! (x, y, z)',
+        missing_args2 = '¡Todos los argumentos tienen que estar presente!',
+        no_access = '¡No tienes acceso a este comando!',
         company_too_poor = 'Tu empresa está en bancarrota. No hay dinero suficiente.',
         item_not_exist = 'El objeto no existe en el sistema.',
         too_heavy = 'Tienes el inventario muy lleno.'
@@ -14,8 +14,11 @@ local Translations = {
         received_paycheck = 'Has recibido tu pago de $%{value}',
         job_info = 'Trabajo: %{value} | Grado: %{value2} | Estado: %{value3}',
         gang_info = 'Pandilla: %{value} | Grado: %{value2}',
-        on_duty = 'Ahora estás en servicio!',
-        off_duty = 'Ahora estás fuera de servicio!'
+        on_duty = '¡Ahora estás en servicio!',
+        off_duty = '¡Ahora estás fuera de servicio!'
+        checking_ban = 'Hola %s. Estámos revisando si has sido baneado.',
+        join_server = 'Bienvenido %s a {Server Name}.',
+        checking_whitelisted = 'Hola %s. Estámos revisando si estas en lista blanca.'
     }
 }
 
