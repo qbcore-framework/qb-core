c2320abb2a94306d8cfdc86601f2abc4b5a65436
buddizer <lucasg@live.com>
2022-05-05 00:50:16 -0600

    Update es.lua

--- locale/es.lua
+++ locale/es.lua
@@ -1,21 +1,21 @@
 local Translations = {
     error = {
-        not_online = 'El jugador no está conectado!',
-        wrong_format = 'Formato incorrecto!',
-        missing_args = 'No todos los argumentos estan presente! (x, y, z)',
-        missing_args2 = 'Todos los argumentos tienen que estar presente!',
-        no_access = 'No tienes acceso a este comando!',
-        company_too_poor = 'Tu empresa está en bancarrota. No hay dinero suficiente.',
-        item_not_exist = 'El objeto no existe en el sistema.',
-        too_heavy = 'Tienes el inventario muy lleno.'
+        not_online = 'El jugador no está en línea',
+        wrong_format = 'Formato incorrecto',
+        missing_args = 'No se han ingresado todos los argumentos (x, y, z)',
+        missing_args2 = '¡Todos los argumentos deben estar presentes!',
+        no_access = 'No tienes acceso a este comando',
+        company_too_poor = 'Tu empleador está en bancarrota',
+        item_not_exist = 'El objeto no existe',
+        too_heavy = 'Tienes el inventario muy lleno'
     },
     success = {},
     info = {
-        received_paycheck = 'Has recibido tu pago de $%{value}',
-        job_info = 'Trabajo: %{value} | Grado: %{value2} | Estado: %{value3}',
-        gang_info = 'Pandilla: %{value} | Grado: %{value2}',
-        on_duty = 'Ahora estás en servicio!',
-        off_duty = 'Ahora estás fuera de servicio!'
+        received_paycheck = 'Has recibido tu salario de $%{value}',
+        job_info = 'Trabajo: %{value} | Puesto: %{value2} | Estado: %{value3}',
+        gang_info = 'Pandilla: %{value} | Puesto: %{value2}',
+        on_duty = '¡Ahora estás en servicio!',
+        off_duty = '¡Ahora estás fuera de servicio!'
     }
 }
 
