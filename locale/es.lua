6f44dc6de822da5fa625a12e00023c6046d64b5b
buddizer <lucasg@live.com>
2022-05-05 00:54:47 -0600

    Update es.lua

--- locale/es.lua
+++ locale/es.lua
@@ -1,9 +1,9 @@
 local Translations = {
     error = {
-        not_online = 'El jugador no está conectado',
+        not_online = 'El jugador no está en línea',
         wrong_format = 'Formato incorrecto',
-        missing_args = 'No has ingresado todos los argumentos (x, y, z)',
-        missing_args2 = '¡Todos los argumentos tienen que estar presentes!',
+        missing_args = 'No se han ingresado todos los argumentos (x, y, z)',
+        missing_args2 = '¡Todos los argumentos deben estar presentes!',
         no_access = 'No tienes acceso a este comando',
         company_too_poor = 'Tu empleador está en bancarrota',
         item_not_exist = 'El objeto no existe',
@@ -14,7 +14,7 @@ local Translations = {
     },
     success = {},
     info = {
-        received_paycheck = 'Has recibido tu pago de $%{value}',
+        received_paycheck = 'Has recibido tu salario de $%{value}',
         job_info = 'Trabajo: %{value} | Puesto: %{value2} | Estado: %{value3}',
         gang_info = 'Pandilla: %{value} | Puesto: %{value2}',
         on_duty = '¡Estás en servicio!',
