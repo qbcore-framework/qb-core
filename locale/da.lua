5fbc72fce166ea61405b6ffdb8f3d44ec1a5f07c
froberg1234 <47919859+7R3C3@users.noreply.github.com>
2022-05-12 03:04:07 +0200

    Fixed da.lua
    Fixed da.lua

--- locale/da.lua
+++ locale/da.lua
@@ -1,21 +1,27 @@
 local Translations = {
     error = {
         not_online = 'Spiller ikke online',
-        wrong_format = 'ugyldigt format',
-        missing_args = 'Alle argumenter er ikke udfyldt (x, y, z)',
-        missing_args2 = 'Alle argumenter skal være udfyldt!',
-        no_access = 'Ingen adgang til denne command',
-        company_too_poor = 'Arbejdsgiveren er for fattig',
-        item_not_exist = 'Enheden eksistere ikke',
-        too_heavy = 'Dit inventory er for fyldt'
+        wrong_format = 'Forkert format',
+        missing_args = 'Ikke alle argumenter er blevet indtastet (x, y, z)',
+        missing_args2 = 'Alle argumenter skal udfyldes!',
+        no_access = 'Ingen adgang til denne kommando',
+        company_too_poor = 'Din arbejdsgiver er i stykker',
+        item_not_exist = 'Ting findes',
+        too_heavy = 'Dit Inventory er fuldt',
+        duplicate_license = 'Duplikeret rockstar licens er fundet',
+        no_valid_license  = 'Ingen gyldig Rockstar-licens fundet',
+        not_whitelisted = 'Du er ikke allowlistet til at spille på denne server'
     },
     success = {},
     info = {
-        received_paycheck = 'Du modtog din lønseddel på %{value} DKK',
+        received_paycheck = 'Du har modtaget din lønseddel på $%{value}',
         job_info = 'Arbejde: %{value} | Grad: %{value2} | Job: %{value3}',
         gang_info = 'Bande: %{value} | Grad: %{value2}',
         on_duty = 'Du er nu på arbejde, god arbejdslyst!',
         off_duty = 'Du er ikke længere på arbejde!'
+        checking_ban = 'Hej %s. Vi ser om du er bannet på denne server.',
+        join_server = 'Velkommen til %s til {Server Name}.',
+        checking_whitelisted = 'Hej %s. Vi ser om du er allowlistet!.'
     }
 }
 
