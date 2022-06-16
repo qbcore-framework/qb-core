bcf4b89a6be2d582f9b60004e70def40f6337e11
Sna-aaa <88613496+Sna-aaa@users.noreply.github.com>
2022-06-16 17:55:29 +0200

    Update nl.lua

--- locale/nl.lua
+++ locale/nl.lua
@@ -31,9 +31,9 @@ local Translations = {
         phrases = Translations,
         warnOnMissing = true,
         locale = "nl"
-    }) 
+    })
     if templocale then
         Lang = templocale
     end
-    
+
 --end                         --Comment this line also
