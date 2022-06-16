8a2cd7d9ce772cbb9d09c32fd789d4580152a2f1
Sna-aaa <88613496+Sna-aaa@users.noreply.github.com>
2022-06-16 17:45:10 +0200

    Update cn.lua

--- locale/cn.lua
+++ locale/cn.lua
@@ -31,9 +31,9 @@ local Translations = {
         phrases = Translations,
         warnOnMissing = true,
         locale = "cn"
-    }) 
+    })
     if templocale then
         Lang = templocale
     end
-    
+
 --end                         --Comment this line also
