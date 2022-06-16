6447bd3e816e8c492efc255244947c5428fed1f5
Sna-aaa <88613496+Sna-aaa@users.noreply.github.com>
2022-06-16 17:55:07 +0200

    Update lv.lua

--- locale/lv.lua
+++ locale/lv.lua
@@ -25,9 +25,9 @@ local Translations = {
         phrases = Translations,
         warnOnMissing = true,
         locale = "lv"
-    }) 
+    })
     if templocale then
         Lang = templocale
     end
-    
+
 --end                         --Comment this line also
