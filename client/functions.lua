35c8ed958fc1442e53b74deb65729d09ef4bd141
Niunisas <55489629+niunisas@users.noreply.github.com>
2022-06-30 21:19:10 +0300

    fix(client/functions): item checking fix

--- client/functions.lua
+++ client/functions.lua
@@ -34,7 +34,7 @@ function QBCore.Functions.HasItem(items, amount)
                 return true
             end
         else -- Single item as string
-            if itemData and itemData.name == items and (not amount or (amount and itemData.amount >= amount)) then
+            if itemData and itemData.name == items and (not amount or (itemData and amount and itemData.amount >= amount)) then
                 return true
             end
         end
