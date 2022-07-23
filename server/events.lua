8d73a69d7b399d617712a72df47e63eef50a0f59
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-07-23 23:49:53 +0300

    chore(server/events): remove unnecessary _
    Co-authored-by: David Malchin <malchin459@gmail.com>

--- server/events.lua
+++ server/events.lua
@@ -169,7 +169,7 @@ RegisterNetEvent('QBCore:Server:UseItem', function(item)
 end)
 
 -- This event is exploitable and should not be used. Will be deprecated soon. function(itemName, amount, slot)
-RegisterNetEvent('QBCore:Server:RemoveItem', function(itemName, amount, _)
+RegisterNetEvent('QBCore:Server:RemoveItem', function(itemName, amount)
     local src = source
     print(string.format("%s triggered QBCore:Server:RemoveItem by ID %s for %s %s. This event is being deprecated because of exploitation. Adjust your events accordingly to do this server side with player functions.", GetInvokingResource(), src, amount, itemName))
 end)
