656b1d56c5f261cd9528447478629c09684c33a8
r0adra93 <github.com@legendsclan.org>
2022-09-04 20:28:17 -0400

    Update jobs.lua
    reset to default

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -12,7 +12,7 @@ QBShared.Jobs = {
             },
         },
 	},
-    ["police"] = {
+	['police'] = {
 		label = 'Law Enforcement',
         type = "leo",
 		defaultDuty = true,
@@ -40,8 +40,8 @@ QBShared.Jobs = {
                 payment = 150
             },
         },
-    },
-    ['ambulance'] = {
+	},
+	['ambulance'] = {
 		label = 'EMS',
 		defaultDuty = true,
 		offDutyPay = false,
@@ -69,7 +69,7 @@ QBShared.Jobs = {
             },
         },
 	},
-    ['realestate'] = {
+	['realestate'] = {
 		label = 'Real Estate',
 		defaultDuty = true,
 		offDutyPay = false,
@@ -97,7 +97,7 @@ QBShared.Jobs = {
             },
         },
 	},
-    ['taxi'] = {
+	['taxi'] = {
 		label = 'Taxi',
 		defaultDuty = true,
 		offDutyPay = false,
@@ -125,159 +125,160 @@ QBShared.Jobs = {
             },
         },
 	},
-    ['bus'] = {
-       label = 'Bus',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Driver',
-               payment = 50
-           },
-       },
-   },
-   ['cardealer'] = {
-       label = 'Vehicle Dealer',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Recruit',
-               payment = 50
-           },
-           ['1'] = {
-               name = 'Showroom Sales',
-               payment = 75
-           },
-           ['2'] = {
-               name = 'Business Sales',
-               payment = 100
-           },
-           ['3'] = {
-               name = 'Finance',
-               payment = 125
-           },
-           ['4'] = {
-               name = 'Manager',
-               isboss = true,
-               payment = 150
-           },
-       },
-   },
-   ['mechanic'] = {
-       label = 'Mechanic',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Recruit',
-               payment = 50
-           },
-           ['1'] = {
-               name = 'Novice',
-               payment = 75
-           },
-           ['2'] = {
-               name = 'Experienced',
-               payment = 100
-           },
-           ['3'] = {
-               name = 'Advanced',
-               payment = 125
-           },
-           ['4'] = {
-               name = 'Manager',
-               isboss = true,
-               payment = 150
-           },
-       },
-   },
-   ['judge'] = {
-       label = 'Honorary',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Judge',
-               payment = 100
-           },
-       },
-   },
-   ['lawyer'] = {
-       label = 'Law Firm',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Associate',
-               payment = 50
-           },
-       },
-   },
-   ['reporter'] = {
-       label = 'Reporter',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Journalist',
-               payment = 50
-           },
-       },
-   },
-   ['trucker'] = {
-       label = 'Trucker',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Driver',
-               payment = 50
-           },
-       },
-   },
-   ['tow'] = {
-       label = 'Towing',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Driver',
-               payment = 50
-           },
-       },
-   },
-   ['garbage'] = {
-       label = 'Garbage',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Collector',
-               payment = 50
-           },
-       },
-   },
-   ['vineyard'] = {
-       label = 'Vineyard',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Picker',
-               payment = 50
-           },
-       },
-   },
-   ['hotdog'] = {
-       label = 'Hotdog',
-       defaultDuty = true,
-       offDutyPay = false,
-       grades = {
-           ['0'] = {
-               name = 'Sales',
-               payment = 50
-           },
-       },
-   },
+     ['bus'] = {
+		label = 'Bus',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Driver',
+                payment = 50
+            },
+		},
+	},
+	['cardealer'] = {
+		label = 'Vehicle Dealer',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Recruit',
+                payment = 50
+            },
+			['1'] = {
+                name = 'Showroom Sales',
+                payment = 75
+            },
+			['2'] = {
+                name = 'Business Sales',
+                payment = 100
+            },
+			['3'] = {
+                name = 'Finance',
+                payment = 125
+            },
+			['4'] = {
+                name = 'Manager',
+				isboss = true,
+                payment = 150
+            },
+        },
+	},
+	['mechanic'] = {
+		label = 'Mechanic',
+        type = "mechanic",
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Recruit',
+                payment = 50
+            },
+			['1'] = {
+                name = 'Novice',
+                payment = 75
+            },
+			['2'] = {
+                name = 'Experienced',
+                payment = 100
+            },
+			['3'] = {
+                name = 'Advanced',
+                payment = 125
+            },
+			['4'] = {
+                name = 'Manager',
+				isboss = true,
+                payment = 150
+            },
+        },
+	},
+	['judge'] = {
+		label = 'Honorary',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Judge',
+                payment = 100
+            },
+        },
+	},
+	['lawyer'] = {
+		label = 'Law Firm',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Associate',
+                payment = 50
+            },
+        },
+	},
+	['reporter'] = {
+		label = 'Reporter',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Journalist',
+                payment = 50
+            },
+        },
+	},
+	['trucker'] = {
+		label = 'Trucker',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Driver',
+                payment = 50
+            },
+        },
+	},
+	['tow'] = {
+		label = 'Towing',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Driver',
+                payment = 50
+            },
+        },
+	},
+	['garbage'] = {
+		label = 'Garbage',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Collector',
+                payment = 50
+            },
+        },
+	},
+	['vineyard'] = {
+		label = 'Vineyard',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Picker',
+                payment = 50
+            },
+        },
+	},
+	['hotdog'] = {
+		label = 'Hotdog',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Sales',
+                payment = 50
+            },
+        },
+	},
 }
