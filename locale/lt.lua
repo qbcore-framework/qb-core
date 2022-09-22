3c588888e0959e5229bb045fced99b6fba3acd85
Dalgelis <109312910+Dalgelis@users.noreply.github.com>
2022-09-22 17:24:20 +0300

    Update lt.lua

--- locale/lt.lua
+++ locale/lt.lua
@@ -118,7 +118,10 @@ local Translations = {
     },
 }
 
-Lang = Lang or Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'lt' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
