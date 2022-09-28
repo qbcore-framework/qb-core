5846df19c0d5f2722849a900e5f445f734b04236
r0adra93 <github.com@legendsclan.org>
2022-09-28 16:26:16 -0400

    updated jobs.lua
    Work in progress showing an idea what is being done.

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -41,62 +41,65 @@ QBShared.Jobs = {
             },
         },
         ["Locations"] = {
-           ["duty"] = {
-               [1] = vector3(440.085, -974.924, 30.689),
-               [2] = vector3(-449.811, 6012.909, 31.815),
-           },
-           ["aircraft"] = {
-               [1] = vector4(449.168, -981.325, 43.691, 87.234),
-               [2] = vector4(-475.43, 5988.353, 31.716, 31.34),
-           },
-           ["landcraft"] = {
-               [1] = vector4(448.159, -1017.41, 28.562, 90.654),
-               [2] = vector4(471.13, -1024.05, 28.17, 274.5),
-               [3] = vector4(-455.39, 6002.02, 31.34, 87.93),
-           },
-           ["watercraft"] = {
-           },
-           ["stash"] = {
-               [1] = vector3(453.075, -980.124, 30.889),
-           },
-           ["impound"] = {
-               [1] = vector3(436.68, -1007.42, 27.32),
-               [2] = vector3(-436.14, 5982.63, 31.34),
-           },
-           ["armory"] = {
-               [1] = vector3(462.23, -981.12, 30.68),
-           },
-           ["trash"] = {
-               [1] = vector3(439.0907, -976.746, 30.776),
-           },
-           ["fingerprint"] = {
-               [1] = vector3(460.9667, -989.180, 24.92),
-           },
-           ["evidence"] = {
-               [1] = vector3(442.1722, -996.067, 30.689),
-               [2] = vector3(451.7031, -973.232, 30.689),
-               [3] = vector3(455.1456, -985.462, 30.689),
-           },
-           ["stations"] = {
-               [1] = {label = "Police Station", coords = vector4(428.23, -984.28, 29.76, 3.5)},
-               [2] = {label = "Prison", coords = vector4(1845.903, 2585.873, 45.672, 272.249)},
-               [3] = {label = "Police Station Paleto", coords = vector4(-451.55, 6014.25, 31.716, 223.81)},
-           },
+            ["duty"] = {
+                [1] = vector3(440.085, -974.924, 30.689),
+                [2] = vector3(-449.811, 6012.909, 31.815),
+            },
+            ["aircraft"] = {
+                [1] = vector4(449.168, -981.325, 43.691, 87.234),
+                [2] = vector4(-475.43, 5988.353, 31.716, 31.34),
+            },
+            ["landcraft"] = {
+                [1] = vector4(448.159, -1017.41, 28.562, 90.654),
+                [2] = vector4(471.13, -1024.05, 28.17, 274.5),
+                [3] = vector4(-455.39, 6002.02, 31.34, 87.93),
+            },
+            ["watercraft"] = {
+            },
+            ["stash"] = {
+                [1] = vector3(453.075, -980.124, 30.889),
+            },
+            ["impound"] = { -- copy vector3 info for  from qb-garages/config.inc
+                [1] = vector3(436.68, -1007.42, 27.32),
+                [2] = vector3(-436.14, 5982.63, 31.34),
+            },
+            ["armory"] = {
+                [1] = vector3(462.23, -981.12, 30.68),
+            },
+            ["clothingrooms"] = {
+                [1] = {requiredJob = 'police', isGang = false, coords = vector3(454.43, -988.85, 30.69), width = 2, length = 2, cameraLocation = vector4(454.42, -990.52, 30.69, 358.48)},
+            },
+            ["trash"] = {
+                [1] = vector3(439.0907, -976.746, 30.776),
+            },
+            ["fingerprint"] = {
+                [1] = vector3(460.9667, -989.180, 24.92),
+            },
+            ["evidence"] = {
+                [1] = vector3(442.1722, -996.067, 30.689),
+                [2] = vector3(451.7031, -973.232, 30.689),
+                [3] = vector3(455.1456, -985.462, 30.689),
+            },
+            ["stations"] = {
+                [1] = {label = "Police Station", coords = vector4(428.23, -984.28, 29.76, 3.5)},
+                [2] = {label = "Prison", coords = vector4(1845.903, 2585.873, 45.672, 272.249)},
+                [3] = {label = "Police Station Paleto", coords = vector4(-451.55, 6014.25, 31.716, 223.81)},
+            },
        },
        ["vehicles"] = {
            ["aircraft"] = {
-       
+
            },
            ["landcraft"] = {
-       
+
            },
            ["watercraft"]= {
-       
+
            },
        },
        ["items"] = {
            ["stash"] = {
-       
+
            },
            ["armory"] = {
                label = "Police Armory",
@@ -280,7 +283,7 @@ QBShared.Jobs = {
                        authorizedJobGrades = {0, 1, 2, 3, 4}
                    }
                }
-       
+
            },
            ["landcraft"] = {
                [1] = {
@@ -306,7 +309,7 @@ QBShared.Jobs = {
                },
            },
            ["landcraft-gove"] = {
-       
+
            },
            ["aircraft"] = {
                [1] = {
@@ -332,7 +335,7 @@ QBShared.Jobs = {
                },
            },
            ["aircraft-gove"] = {
-       
+
            },
            ["watercraft"] = {
                [1] = {
@@ -358,7 +361,7 @@ QBShared.Jobs = {
                },
            },
            ["watercraft-gove"] = {
-       
+
            },
        },
 	},
