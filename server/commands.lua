4c553d6155d0603a403fc68b8dc55b959b8a2bed
Marshy <84200284+Marshxan@users.noreply.github.com>
2022-10-25 23:14:45 +0100

    Update commands.lua

--- server/commands.lua
+++ server/commands.lua
@@ -290,7 +290,7 @@ QBCore.Commands.Add('me', Lang:t("command.me.help"), {{name = Lang:t("command.me
         local tCoords = GetEntityCoords(target)
         if target == ped or #(pCoords - tCoords) < 20 then
             TriggerClientEvent('QBCore:Command:ShowMe3D', Player, source, msg)
-                TriggerEvent('qb-log:server:CreateLog', 'me', Player, 'lightgreen', msg)
+            TriggerEvent('qb-log:server:CreateLog', 'me', 'Me Command Log', 'white', '**ID: ' .. source .. ') Message:** ' .. msg, false)
         end
     end
 end, 'user')
