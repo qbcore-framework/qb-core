312f24d245a6cf66d45f9f4fcb2f52838a2b361c
Chris Sinclair <18537102+MannaMuncher@users.noreply.github.com>
2022-12-11 04:43:10 -0800

    Bypass for loading QBCore in FxDK (#792)
    FxDK does not include license identifiers by default and thus breaks scripts that utilize those. Without this you will get the "No valid Rockstar license" error when connecting. This fix was not my own I found it in the post linked below and tested myself.
    
    https://forum.cfx.re/t/connection-rejected-by-server-fxdk/2504365/5
    
    Co-authored-by: Kakarot <57848836+GhzGarage@users.noreply.github.com>
    Co-authored-by: Stan <96954031+tom-osborne@users.noreply.github.com>
    Co-authored-by: Zerio <54480523+Z3rio@users.noreply.github.com>

--- server/events.lua
+++ server/events.lua
@@ -41,6 +41,10 @@ local function onPlayerConnecting(name, _, deferrals)
         end
     end
 
+    if GetConvarInt("sv_fxdkMode", false) then
+        license = 'license:AAAAAAAAAAAAAAAA' -- Dummy License
+    end
+
     if not license then
         deferrals.done(Lang:t('error.no_valid_license'))
     elseif QBCore.Config.Server.CheckDuplicateLicense and QBCore.Functions.IsLicenseInUse(license) then
