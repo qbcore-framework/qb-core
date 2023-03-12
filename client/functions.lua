9ae5749beb98245d5657c91c62b24fed9acb7b9e
Inspector-Parrot <127422038+Inspector-Parrot@users.noreply.github.com>
2023-03-12 19:58:51 +0100

    QBCore.Functions.DrawText Update
    The changes made to QBCore.Functions.DrawText were done to optimize the function and make it more readable and maintainable.

--- client/functions.lua
+++ client/functions.lua
@@ -19,15 +19,9 @@ end
 -- Utility
 
 function QBCore.Functions.DrawText(x, y, width, height, scale, r, g, b, a, text)
-    -- Use local function instead
     SetTextFont(4)
-    SetTextProportional(0)
     SetTextScale(scale, scale)
     SetTextColour(r, g, b, a)
-    SetTextDropShadow(0, 0, 0, 0, 255)
-    SetTextEdge(2, 0, 0, 0, 255)
-    SetTextDropShadow()
-    SetTextOutline()
     SetTextEntry('STRING')
     AddTextComponentString(text)
     DrawText(x - width / 2, y - height / 2 + 0.005)
