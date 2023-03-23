6738c06d5502a46bb73b0e56ba1de1b6afb94fed
Hakos47 <76844995+hakos47@users.noreply.github.com>
2023-03-23 15:02:35 +0100

    Update functions.lua

--- client/functions.lua
+++ client/functions.lua
@@ -996,6 +996,6 @@ end
 function QBCore.Functions.GetGroundHash(entity)
     local coords = GetEntityCoords(entity)
     local num = StartShapeTestCapsule(coords.x,coords.y,coords.z+4,coords.x,coords.y,coords.z-2.0, 1,1,entity,7)
-    local retval --[[ integer ]], hit --[[ boolean ]], endCoords --[[ vector3 ]], surfaceNormal --[[ vector3 ]], materialHash --[[ Hash ]], entityHit --[[ Entity ]] = GetShapeTestResultEx(num)
-    return materialHash, entityHit, surfaceNormal, endCoords, hit, retval
+    local retval , success , endCoords, surfaceNormal, materialHash , entityHit  = GetShapeTestResultEx(num)
+    return materialHash, entityHit, surfaceNormal, endCoords, success, retval
 end
