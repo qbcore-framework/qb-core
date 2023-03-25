4ba2262a27f11b297729ffc472ebd7c5626422c1
Zerio <54480523+Z3rio@users.noreply.github.com>
2023-03-25 14:19:51 +0100

    Improve code readability

--- client/functions.lua
+++ client/functions.lua
@@ -995,7 +995,7 @@ end
 
 function QBCore.Functions.GetGroundHash(entity)
     local coords = GetEntityCoords(entity)
-    local num = StartShapeTestCapsule(coords.x,coords.y,coords.z+4,coords.x,coords.y,coords.z-2.0, 1,1,entity,7)
-    local retval , success , endCoords, surfaceNormal, materialHash , entityHit  = GetShapeTestResultEx(num)
+    local num = StartShapeTestCapsule(coords.x, coords.y, coords.z + 4, coords.x, coords.y, coords.z - 2.0, 1, 1, entity, 7)
+    local retval, success, endCoords, surfaceNormal, materialHash, entityHit = GetShapeTestResultEx(num)
     return materialHash, entityHit, surfaceNormal, endCoords, success, retval
 end
