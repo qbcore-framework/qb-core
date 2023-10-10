5015a2f78d564dafecfb84414793d951f7daab19
Stevie <djstevie91@live.com>
2023-10-10 09:13:08 +0100

    Update gangs.lua
    fix linting issue

--- shared/gangs.lua
+++ shared/gangs.lua
@@ -1,46 +1,46 @@
 QBShared = QBShared or {}
 QBShared.Gangs = {
-    ['none'] = createGang('No Gang', {
-        ['0'] = { name = 'Unaffiliated' }
-    }),
-    ['lostmc'] = createGang('The Lost MC', {
-        ['0'] = { name = 'Recruit' },
-        ['1'] = { name = 'Enforcer' },
-        ['2'] = { name = 'Shot Caller' },
-        ['3'] = { name = 'Boss', isboss = true }
-    }),
-    ['ballas'] = createGang('Ballas', {
-        ['0'] = { name = 'Recruit' },
-        ['1'] = { name = 'Enforcer' },
-        ['2'] = { name = 'Shot Caller' },
-        ['3'] = { name = 'Boss', isboss = true }
-    }),
-    ['vagos'] = createGang('Vagos', {
-        ['0'] = { name = 'Recruit' },
-        ['1'] = { name = 'Enforcer' },
-        ['2'] = { name = 'Shot Caller' },
-        ['3'] = { name = 'Boss', isboss = true }
-    }),
-    ['cartel'] = createGang('Cartel', {
-        ['0'] = { name = 'Recruit' },
-        ['1'] = { name = 'Enforcer' },
-        ['2'] = { name = 'Shot Caller' },
-        ['3'] = { name = 'Boss', isboss = true }
-    }),
-    ['families'] = createGang('Families', {
-        ['0'] = { name = 'Recruit' },
-        ['1'] = { name = 'Enforcer' },
-        ['2'] = { name = 'Shot Caller' },
-        ['3'] = { name = 'Boss', isboss = true }
-    }),
-    ['triads'] = createGang('Triads', {
-        ['0'] = { name = 'Recruit' },
-        ['1'] = { name = 'Enforcer' },
-        ['2'] = { name = 'Shot Caller' },
-        ['3'] = { name = 'Boss', isboss = true }
-    })
+	['none'] = createGang('No Gang', {
+		['0'] = { name = 'Unaffiliated' }
+	}),
+	['lostmc'] = createGang('The Lost MC', {
+		['0'] = { name = 'Recruit' },
+		['1'] = { name = 'Enforcer' },
+		['2'] = { name = 'Shot Caller' },
+		['3'] = { name = 'Boss', isboss = true }
+	}),
+	['ballas'] = createGang('Ballas', {
+		['0'] = { name = 'Recruit' },
+		['1'] = { name = 'Enforcer' },
+		['2'] = { name = 'Shot Caller' },
+		['3'] = { name = 'Boss', isboss = true }
+	}),
+	['vagos'] = createGang('Vagos', {
+		['0'] = { name = 'Recruit' },
+		['1'] = { name = 'Enforcer' },
+		['2'] = { name = 'Shot Caller' },
+		['3'] = { name = 'Boss', isboss = true }
+	}),
+	['cartel'] = createGang('Cartel', {
+		['0'] = { name = 'Recruit' },
+		['1'] = { name = 'Enforcer' },
+		['2'] = { name = 'Shot Caller' },
+		['3'] = { name = 'Boss', isboss = true }
+	}),
+	['families'] = createGang('Families', {
+		['0'] = { name = 'Recruit' },
+		['1'] = { name = 'Enforcer' },
+		['2'] = { name = 'Shot Caller' },
+		['3'] = { name = 'Boss', isboss = true }
+	}),
+	['triads'] = createGang('Triads', {
+		['0'] = { name = 'Recruit' },
+		['1'] = { name = 'Enforcer' },
+		['2'] = { name = 'Shot Caller' },
+		['3'] = { name = 'Boss', isboss = true }
+	})
 }
 
 local function createGang(label, grades)
-    return { label = label, grades = grades }
+	return { label = label, grades = grades }
 end
