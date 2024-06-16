fd3ccc7e36bcaca6c8ce3395dee69a16719e001c
EinS4ckZwiebeln <66690565+EinS4ckZwiebeln@users.noreply.github.com>
2024-06-16 18:09:54 +0200

    route event logic through internal functions

--- client/events.lua
+++ client/events.lua
@@ -121,24 +121,18 @@ end)
 RegisterNetEvent('QBCore:Command:SpawnVehicle', function(vehName)
     local ped = PlayerPedId()
     local hash = joaat(vehName)
-    local veh = GetVehiclePedIsUsing(ped)
     if not IsModelInCdimage(hash) then return end
-    RequestModel(hash)
-    while not HasModelLoaded(hash) do
-        Wait(0)
-    end
-
+    QBCore.Functions.LoadModel(hash)
     if IsPedInAnyVehicle(ped) then
-        SetEntityAsMissionEntity(veh, true, true)
-        DeleteVehicle(veh)
+        local currentVehicle = GetVehiclePedIsUsing(ped)
+        SetEntityAsMissionEntity(currentVehicle, true, true)
+        DeleteVehicle(currentVehicle)
     end
-
-    local vehicle = CreateVehicle(hash, GetEntityCoords(ped), GetEntityHeading(ped), true, false)
-    TaskWarpPedIntoVehicle(ped, vehicle, -1)
-    SetVehicleFuelLevel(vehicle, 100.0)
-    SetVehicleDirtLevel(vehicle, 0.0)
-    SetModelAsNoLongerNeeded(hash)
-    TriggerEvent('vehiclekeys:client:SetOwner', QBCore.Functions.GetPlate(vehicle))
+    QBCore.Functions.SpawnVehicle(hash, function(vehicle)
+        SetEntityHeading(vehicle, GetEntityHeading(ped))
+        SetVehicleDirtLevel(vehicle, 0.0)
+        TriggerEvent('vehiclekeys:client:SetOwner', QBCore.Functions.GetPlate(vehicle))
+    end, GetEntityCoords(ped), true, true)
 end)
 
 RegisterNetEvent('QBCore:Command:DeleteVehicle', function()
