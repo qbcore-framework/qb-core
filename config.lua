06f7f6e96764155714ca68b6edcbc3afb370e009
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-10-22 15:06:17 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,7 +8,7 @@ QBConfig.StatusInterval = 5000                          -- how often to check hu
 QBConfig.Money = {}
 QBConfig.Money.MoneyTypes = { cash = 500, bank = 5000, crypto = 0 } -- type = startamount - Add or remove money types for your server (for ex. blackmoney = 0), remember once added it will not be removed from the database!
 QBConfig.Money.DontAllowMinus = { 'cash', 'crypto' }                -- Money that is not allowed going in minus
-QBConfig.Money.MinusLimit = 5000                                    -- The maximum amount you can be negative 
+QBConfig.Money.MinusLimit = -5000                                    -- The maximum amount you can be negative 
 QBConfig.Money.PayCheckTimeOut = 10                                 -- The time in minutes that it will give the paycheck
 QBConfig.Money.PayCheckSociety = false                              -- If true paycheck will come from the society account that the player is employed at, requires qb-management
 
