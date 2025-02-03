289811cd54904bd262e3b795d6c010cd0777790e
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2025-02-03 10:21:12 +0000

    Refactor duty count retrieval and add duty info calculation function

--- client/functions.lua
+++ client/functions.lua
@@ -354,12 +354,25 @@ function QBCore.Functions.GetClosestObject(coords)
     return closestObject, closestDistance
 end
 
-function QBCore.Functions.GetDutyCount(jobstring, isType)
-    local result = QBCore.Functions.TriggerCallback('QBCore:Server:GetDutyCount', jobstring, isType)
-    return result
+function QBCore.Functions.GetDutyCount(jobstring, istype)
+    return GlobalState.dutyInfo[istype and 'type_'..jobstring or jobstring] or 0
 end
 
-exports('GetDutyCount', QBCore.Functions.GetDutyCount)
+--- Recalculate the duty info for all players
+--- @param exclude number?
+function QBCore.Functions.CalculateDutyInfo(exclude)
+    if exclude then exlude = tonumber(exclude) end
+    if not GlobalState.dutyInfo then GlobalState.dutyInfo = {} end
+    local dutyInfo = {}
+    for _, Player in pairs(QBCore.Players) do
+        if exclude and exlude == Player.PlayerData.source then goto continue end
+        local job = Player.PlayerData.job
+        dutyInfo[job.name] = (dutyInfo[job.name] or 0) + (job.onduty and 1 or 0)
+        if job.type and job.type ~= 'none' then dutyInfo['type_'..job.type] = (dutyInfo['type_'..job.type] or 0) + (job.onduty and 1 or 0) end
+        GlobalState:set('dutyInfo', dutyInfo, true)
+        ::continue::
+    end
+end
 
 -- Vehicle
 
