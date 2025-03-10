de9021c7387f009d1f9c0338782d28b0e7bac3e1
Joel <34402846+Qwerty1Verified@users.noreply.github.com>
2025-03-10 23:27:54 +0000

    fix: add qb_locale convar check

--- locale/ua.lua
+++ locale/ua.lua
@@ -123,8 +123,9 @@ local Translations = {
         },
     },
 }
-
-Lang = Lang or Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'ua' then
+    Lang = Lang or Locale:new({
+        phrases = Translations,
+        warnOnMissing = true
+    })
+end
