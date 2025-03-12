493d5b7279011fff823bd4c2a1eac3b7581a612e
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-03-11 23:34:35 -0500

    editor callbacks

---
+++ client/editor.lua
@@ -0,0 +1,20 @@
+RegisterNUICallback('saveConfig', function(data, cb)
+    local file = data.file
+    local configData = data.data
+    TriggerServerEvent('qb-core:server:configEditor', file, configData)
+    print("Saving config for file:", file)
+    cb('ok')
+end)
+
+RegisterNUICallback('closeEditor', function(data, cb)
+    SetNuiFocus(false, false)
+    cb('ok')
+end)
+
+RegisterNetEvent('qb-core:client:configEditor', function(allData)
+    SendNUIMessage({
+        action = 'populateData',
+        data = allData
+    })
+    SetNuiFocus(true, true)
+end)
