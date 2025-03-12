e7ddd55af42da5b24d072f756431f495628c1022
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-03-11 23:07:28 -0500

    Update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,49 +2,37 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 author 'Kakarot'
-description 'Core resource for the framework, contains all the core functionality and features'
-version '1.3.0'
+description 'Core resource for the framework'
+version '2.0.0'
 
 shared_scripts {
-    'config.lua',
     'shared/locale.lua',
-    'locale/en.lua',
     'locale/*.lua',
-    'shared/main.lua',
-    'shared/items.lua',
-    'shared/jobs.lua',
-    'shared/vehicles.lua',
-    'shared/gangs.lua',
-    'shared/weapons.lua',
-    'shared/locations.lua'
+    'shared/lua/core.lua',
+    'shared/lua/loader.lua',
+    'shared/lua/functions.lua',
 }
 
 client_scripts {
-    'client/main.lua',
-    'client/functions.lua',
-    'client/loops.lua',
-    'client/events.lua',
-    'client/drawtext.lua'
+    'client/*.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua',
-    'server/functions.lua',
-    'server/player.lua',
-    'server/events.lua',
-    'server/commands.lua',
-    'server/exports.lua',
-    'server/debug.lua'
+    'server/*.lua',
 }
 
 ui_page 'html/index.html'
 
 files {
     'html/index.html',
-    'html/css/style.css',
-    'html/css/drawtext.css',
-    'html/js/*.js'
+    'html/style.css',
+    'html/script.js',
+    'shared/json/jobs.json',
+    'shared/json/gangs.json',
+    'shared/json/items.json',
+    'shared/json/vehicles.json',
+    'shared/json/weapons.json'
 }
 
 dependency 'oxmysql'
