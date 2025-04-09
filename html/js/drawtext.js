d0e9606efff31f8a078fb62ef032ec6048baa51f
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2025-04-09 21:43:59 +0100

    refactor: improve documentation and formatting in drawtext.js; remove unused testing.js and utils.js files

--- html/js/drawtext.js
+++ html/js/drawtext.js
@@ -1,159 +1,153 @@
 let direction = null;
 
 /**
- * Draw text on the screen
- * @param {{position: 'left' | 'top' | 'right', text: string}} textData
- * @returns {void}
+ * Draws text on the screen
+ * @param {{text: string, position?: 'left' | 'right' | 'top'}} textData
  */
 const drawText = async (textData) => {
-  const text = document.getElementById("text");
-  let { position } = textData;
-  switch (textData.position) {
-    case "left":
-      addClass(text, position);
-      direction = "left";
-      break;
-    case "top":
-      addClass(text, position);
-      direction = "top";
-      break;
-    case "right":
-      addClass(text, position);
-      direction = "right";
-      break;
-    default:
-      addClass(text, "left");
-      direction = "left";
-      break;
-  }
+    const text = document.getElementById("text");
+    let { position } = textData;
+    switch (textData.position) {
+        case "left":
+            addClass(text, position);
+            direction = "left";
+            break;
+        case "top":
+            addClass(text, position);
+            direction = "top";
+            break;
+        case "right":
+            addClass(text, position);
+            direction = "right";
+            break;
+        default:
+            addClass(text, "left");
+            direction = "left";
+            break;
+    }
 
-  text.innerHTML = textData.text;
-  document.getElementById("drawtext-container").style.display = "block";
-  await sleep(100);
-  addClass(text, "show");
+    text.innerHTML = textData.text;
+    document.getElementById("drawtext-container").style.display = "block";
+    await sleep(100);
+    addClass(text, "show");
 };
 
 /**
- * Change the text on the screen
- * @param {{position: 'left' | 'top' | 'right', text: string}} textData
- * @returns {void}
+ * Changes the text on the screen
+ * @param {{text: string, position?: 'left' | 'right' | 'top'}} textData
  */
 const changeText = async (textData) => {
-  const text = document.getElementById("text");
-  let { position } = textData;
+    const text = document.getElementById("text");
+    let { position } = textData;
 
-  removeClass(text, "show");
-  addClass(text, "pressed");
-  addClass(text, "hide");
+    removeClass(text, "show");
+    addClass(text, "pressed");
+    addClass(text, "hide");
 
-  await sleep(500);
-  removeClass(text, "left");
-  removeClass(text, "right");
-  removeClass(text, "top");
-  removeClass(text, "bottom");
-  removeClass(text, "hide");
-  removeClass(text, "pressed");
-
-  switch (textData.position) {
-    case "left":
-      addClass(text, position);
-      direction = "left";
-      break;
-    case "top":
-      addClass(text, position);
-      direction = "top";
-      break;
-    case "right":
-      addClass(text, position);
-      direction = "right";
-      break;
-    default:
-      addClass(text, "left");
-      direction = "left";
-      break;
-  }
-  text.innerHTML = textData.text;
-
-  await sleep(100);
-  text.classList.add("show");
-};
-
-/**
- * Hide the text on the screen
- * @returns {void}
- */
-const hideText = async () => {
-  const text = document.getElementById("text");
-  removeClass(text, "show");
-  addClass(text, "hide");
-
-  setTimeout(() => {
+    await sleep(500);
     removeClass(text, "left");
     removeClass(text, "right");
     removeClass(text, "top");
     removeClass(text, "bottom");
     removeClass(text, "hide");
     removeClass(text, "pressed");
-    document.getElementById("drawtext-container").style.display = "none";
-  }, 1000);
+
+    switch (textData.position) {
+        case "left":
+            addClass(text, position);
+            direction = "left";
+            break;
+        case "top":
+            addClass(text, position);
+            direction = "top";
+            break;
+        case "right":
+            addClass(text, position);
+            direction = "right";
+            break;
+        default:
+            addClass(text, "left");
+            direction = "left";
+            break;
+    }
+    text.innerHTML = textData.text;
+
+    await sleep(100);
+    text.classList.add("show");
 };
 
 /**
- * Key pressed animation
- * @returns {void}
+ * Hides the displayed text
+ */
+const hideText = async () => {
+    const text = document.getElementById("text");
+    removeClass(text, "show");
+    addClass(text, "hide");
+
+    setTimeout(() => {
+        removeClass(text, "left");
+        removeClass(text, "right");
+        removeClass(text, "top");
+        removeClass(text, "bottom");
+        removeClass(text, "hide");
+        removeClass(text, "pressed");
+        document.getElementById("drawtext-container").style.display = "none";
+    }, 1000);
+};
+
+/**
+ * Handles key press events
  */
 const keyPressed = () => {
-  const text = document.getElementById("text");
-  addClass(text, "pressed");
+    const text = document.getElementById("text");
+    addClass(text, "pressed");
 };
 
 window.addEventListener("message", (event) => {
-  const data = event.data;
-  const action = data.action;
-  const textData = data.data;
-  switch (action) {
-    case "DRAW_TEXT":
-      return drawText(textData);
-    case "CHANGE_TEXT":
-      return changeText(textData);
-    case "HIDE_TEXT":
-      return hideText();
-    case "KEY_PRESSED":
-      return keyPressed();
-    default:
-      return;
+    const data = event.data;
+    const action = data.action;
+    const textData = data.data;
+    switch (action) {
+        case "DRAW_TEXT":
+            return drawText(textData);
+        case "CHANGE_TEXT":
+            return changeText(textData);
+        case "HIDE_TEXT":
+            return hideText();
+        case "KEY_PRESSED":
+            return keyPressed();
+        default:
+            return;
     }
 });
 
 /**
- * Sleep for a given amount of time
+ * Sleep function to delay execution for a given number of milliseconds.
  * @param {number} ms
- * @returns
+ * @returns Promise<void>
  */
 const sleep = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
 /**
- * Remove a class from an element
+ * Removes a class from an element if it exists.
  * @param {HTMLElement} element
  * @param {string} name
- * @returns {void}
  */
 const removeClass = (element, name) => {
-  if (element.classList.contains(name)) {
-    element.classList.remove(name);
-  }
+    if (element.classList.contains(name)) {
+        element.classList.remove(name);
+    }
 };
 
 /**
- * Add a class to an element
+ * Adds a class to an element if it doesn't already exist.
  * @param {HTMLElement} element
  * @param {string} name
- * @returns {void}
  */
 const addClass = (element, name) => {
-  if (!element.classList.contains(name)) {
-    element.classList.add(name);
-  }
+    if (!element.classList.contains(name)) {
+        element.classList.add(name);
+    }
 };
